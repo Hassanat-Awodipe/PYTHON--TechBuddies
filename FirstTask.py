d9c8d2cddc3853def99cf5dbd0303be7a956bb10
Hassanat <arsana.awodipe@gmail.com>
2021-12-18 17:57:22 +0100

    solved simple ones first

---
+++ FirstTask.py
@@ -0,0 +1,28 @@
+def simp_add(a, b):
+    print (a + b)
+
+simp_add(2, 3)
+
+
+def simp_sub(a, b):
+    print (a - b)
+
+simp_sub(5, 3)
+
+
+def simp_div(a, b):
+    print (a / b)
+
+simp_div(10, 2)
+
+
+def simp_mul(a, b):
+    print (a * b)   
+
+simp_mul(4, 2)
+
+
+def simp_avg(a, b, c, d, e):
+    print ((a + b + c + d + e) / 5)
+
+simp_avg(1, 2, 3, 4, 5)
